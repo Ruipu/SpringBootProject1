8dba05c6e170649d6b2d3f0dd6f173191067eec6
Ruipu <rgao4@tulane.edu>
2026-06-12 15:17:43 -0500

    Asyn,cache,log.

--- src/main/java/net/javaguides/ems/EmsBackApplication.java
+++ src/main/java/net/javaguides/ems/EmsBackApplication.java
@@ -2,8 +2,12 @@ package net.javaguides.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableCaching
+@EnableAsync
 public class EmsBackApplication {
 
 	public static void main(String[] args) {
--- src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,9 @@
 package net.javaguides.ems.service.impl;
 
+import net.javaguides.ems.service.NotificationService;
+import org.springframework.cache.annotation.Cacheable;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.entity.Employee;
 import net.javaguides.ems.exception.ResourceNotFoundException;
@@ -26,26 +29,39 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
+    private NotificationService notificationService;
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        log.info("Creating employee");
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
+        notificationService.sendNotification();
+        log.info("Employee created successfully");
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
 
     @Override
+    @Cacheable(
+            value = "employees",
+            key = "#employeeId"
+    )
     public EmployeeDto getEmployeeById(Long employeeId) {
+        log.info("Getting employee with id: {}", employeeId);
         Employee employee = employeeRepository.findById(employeeId)
             .orElseThrow(()->
                     new ResourceNotFoundException("Employee not found with id: " + employeeId));
+        log.info("Employee found successfully");
         return EmployeeMapper.mapToEmployeeDto(employee);
     }
 
     @Override
     public List<EmployeeDto> getAllEmployees() {
+        log.info("Getting all employees");
         List<Employee> employees = employeeRepository.findAll();
+        log.info("Retrieved {} employees", employees.size());
         return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
                 .collect(java.util.stream.Collectors.toList());
 
@@ -53,20 +69,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployeeDto) {
+        log.info("Updating employee with id: {}", employeeId);
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(()
                 -> new ResourceNotFoundException("Employee not found with id: " + employeeId));
         employee.setFirstName(updatedEmployeeDto.getFirstName());
         employee.setLastName(updatedEmployeeDto.getLastName());
         employee.setEmail(updatedEmployeeDto.getEmail());
         Employee updatedEmployeeObj = employeeRepository.save(employee);
+        log.info("Employee updated successfully");
         return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
     }
 
     @Override
     public void deleteEmployee(Long employeeId) {
+        log.info("Deleting employee with id: {}", employeeId);
         Employee employee = employeeRepository.findById(employeeId).orElseThrow(
                 ()-> new ResourceNotFoundException("Employee not found with id: " + employeeId)
         );
         employeeRepository.deleteById(employeeId);
+        log.info("Employee deleted successfully");
     }
 }
